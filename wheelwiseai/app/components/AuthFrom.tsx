c927fac090cc8403ff89cb5ce89a222f945dbadf
Danin <msg.danin@gmail.com>
2023-11-21 00:09:21 -0800

    General React calls for Log-in and Registration added

--- wheelwiseai/app/components/AuthFrom.tsx
+++ wheelwiseai/app/components/AuthFrom.tsx
@@ -1,7 +1,68 @@
+'use client';
+
+import { useCallback, useState } from "react";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+type Variant = "LOGIN" | "REGISTER";
+
+
 const AuthForm = () => {
+    
+    const [variant, setVariant] = useState<Variant>('LOGIN');
+    const [isLoading, setIsLoading] = useState(false);
+    const toggleVariant = useCallback(() => {
+
+        if (variant === 'LOGIN'){
+            setVariant('REGISTER')
+        } else {
+            setVariant('LOGIN')
+        }
+
+    },[variant]);
+
+    const {
+        register,
+        handleSubmit,
+        formState: {
+            errors
+        }
+    } = useForm<FieldValues>({
+        defaultValues :{
+            name:'',
+            email:'',
+            password:''
+        }
+    });
+
+    const onSubmit: SubmitHandler<FieldValues> = (data) => {
+        setIsLoading(true);
+
+        if ( variant === 'REGISTER') 
+        {
+            // Ill add the register later here
+        }
+
+        if (variant === 'LOGIN')
+        {
+            // Ill add the login here later
+        }
+    }
+
     return(
-        <div>sssssssssssssssssssssssss</div>
-    )
+
+        // Login element here
+        <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+
+            <div className="bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10">
+
+                ssss
+
+            </div>
+
+        </div>
+    );
+
+
 }
 
 export default AuthForm
+
