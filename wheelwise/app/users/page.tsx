1630283576ac65eec3a34abf4d982fa4f4d7f39b
Danin <msg.danin@gmail.com>
2023-12-05 04:24:00 -0800

    one last bug

--- wheelwise/app/users/page.tsx
+++ wheelwise/app/users/page.tsx
@@ -1,10 +1,13 @@
 'use client'
 
+import { signOut } from "next-auth/react";
+import Button from "../components/Button";
 
 const Users = () => {
   return (
     <div>
       THIS IS USER PAGE 
+      <Button onClick={() => signOut()} type="submit">Log out</Button>
     </div>
    );
 }
