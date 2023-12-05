08a96377cd2713757e8225b13337fba502daf6d7
Danin <msg.danin@gmail.com>
2023-12-05 04:21:27 -0800

    Log in / Log out/ Register - Done

--- wheelwise/app/users/page.tsx
+++ wheelwise/app/users/page.tsx
@@ -1,13 +1,10 @@
 'use client'
 
-import { signOut } from "next-auth/react";
-import Button from "../components/Button";
 
 const Users = () => {
   return (
     <div>
       THIS IS USER PAGE 
-      <Button onClick={() => signOut()} type="submit">Log out</Button>
     </div>
    );
 }
