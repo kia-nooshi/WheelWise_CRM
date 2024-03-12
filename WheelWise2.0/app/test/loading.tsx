0818990df970d8d7d04b92957ffcd2a9c5ee5a0b
Kianoosh Nooshi <kianooshnooshi@Kianooshs-MacBook-Pro-2.local>
2024-03-12 16:35:35 -0700

    loading screen  fixes #98

---
+++ WheelWise2.0/app/test/loading.tsx
@@ -0,0 +1,9 @@
+import { RiLoader4Line } from 'react-icons/ri'
+import React from 'react'
+import { Ui } from '@/components'
+
+const loading = () => {
+   return <Ui.Loading title='Making your test ready' />
+}
+
+export default loading
