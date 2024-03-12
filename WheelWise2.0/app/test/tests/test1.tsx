cae2a8b4bf0d8cbb8aeaa9564218a1b0884ec761
Kianoosh Nooshi <kianooshnooshi@Kianooshs-MacBook-Pro-2.local>
2024-03-12 16:27:59 -0700

    checks clerkID -- user login or not fixes #91

---
+++ WheelWise2.0/app/test/tests/test1.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Test_Section from '../comps/section'
+import Test_Render from '../comps/render'
+import { Util } from '@/components/do'
+import timer from '../comps/timer'
+
+export async function Test_1() {
+   const { result: getClerkId, executionTime: getClerkId_executionTime } = await timer(
+      Util.Clerk.getClerkId
+   )
+
+   return (
+      <Test_Section title='TEST #1 - Clerk Functionality'>
+         <Test_Render
+            label='Clerk.getClerkId'
+            description='Retrieve Clerk ID'
+            data={getClerkId}
+            executionTime={getClerkId_executionTime}
+         />
+      </Test_Section>
+   )
+}
