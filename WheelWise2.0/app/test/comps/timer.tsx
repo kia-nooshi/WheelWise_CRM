473c479a1cef85e95d2e7152736c9b47a0b2688f
Kianoosh Nooshi <kianooshnooshi@Kianooshs-MacBook-Pro-2.local>
2024-03-12 16:26:12 -0700

    calculates execution time of each test fixes #96

---
+++ WheelWise2.0/app/test/comps/timer.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default async function timer(func: any) {
+   const start = performance.now()
+   const result = await func()
+   const end = performance.now()
+   const executionTime = (end - start).toFixed(2) + ' ms'
+   return { result, executionTime }
+}
