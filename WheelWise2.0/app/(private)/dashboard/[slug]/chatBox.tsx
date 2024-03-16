75f8a73da6d584a2736b8c4591c899030c2044f2
Will Huang <willhuang2000@gmail.com>
2024-03-16 16:19:20 -0700

    Form input validation.

--- WheelWise2.0/app/(private)/dashboard/[slug]/chatBox.tsx
+++ WheelWise2.0/app/(private)/dashboard/[slug]/chatBox.tsx
@@ -1,9 +1,22 @@
 import React from 'react'
 
-export default function ChatBox() {
+export default async function ChatBox() {
+  const addMessage = async (formData: FormData) => {
+    'use server'
+
+    const ss = formData.get('message')
+
+    console.log(ss)
+  }
+
   return (
-    <form className='flex flex-row items-center justify-center gap-5 w-full'>
-      <input className='w-full rounded-lg p-2' />
+    <form action={addMessage} className='flex flex-row items-center justify-center gap-5 w-full'>
+      <input
+        name='message'
+        required
+        placeholder='Type your message here ...'
+        className='w-full rounded-lg p-2'
+      />
       <button>Send</button>
     </form>
   )
