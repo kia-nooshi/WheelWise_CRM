72638b12906d8ed7f30e8f4c03cb88d3523efc0f
SiddhartaDutta <siddhartadutta99@gmail.com>
2024-03-18 20:08:33 -0700

    added init imports + const

---
+++ WheelWise2.0/lib/components/ui/ui_shadcn/dropdown-menu.tsx
@@ -0,0 +1,18 @@
+'use client'
+
+import * as DropdownMenuPrimitive from '@radix-ui/react-dropdown-menu'
+import { CheckIcon, ChevronRightIcon, DotFilledIcon } from '@radix-ui/react-icons'
+import * as React from 'react'
+import { Util } from '@/lib'
+
+const DropdownMenu = DropdownMenuPrimitive.Root
+
+const DropdownMenuTrigger = DropdownMenuPrimitive.Trigger
+
+const DropdownMenuGroup = DropdownMenuPrimitive.Group
+
+const DropdownMenuPortal = DropdownMenuPrimitive.Portal
+
+const DropdownMenuSub = DropdownMenuPrimitive.Sub
+
+const DropdownMenuRadioGroup = DropdownMenuPrimitive.RadioGroup
