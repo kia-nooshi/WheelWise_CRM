31efdb6dc2943e21ca215c9c95b2e9dbebd30044
SiddhartaDutta <siddhartadutta99@gmail.com>
2024-03-18 19:50:16 -0700

    exportation + const create; fixes #110

--- WheelWise2.0/lib/components/ui/ui_shadcn/button.tsx
+++ WheelWise2.0/lib/components/ui/ui_shadcn/button.tsx
@@ -29,3 +29,25 @@ const buttonVariants = cva(
     },
   }
 )
+
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {
+  asChild?: boolean
+}
+
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, variant, size, asChild = false, ...props }, ref) => {
+    const Comp = asChild ? Slot : 'button'
+    return (
+      <Comp
+        className={Util.Other.cn(buttonVariants({ variant, size, className }))}
+        ref={ref}
+        {...props}
+      />
+    )
+  }
+)
+Button.displayName = 'Button'
+
+export { Button, buttonVariants }
